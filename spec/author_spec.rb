2657d5be57ef5e6ecd89fd42e1ee1c0f19762958
AdornChoga <adornchoga@gmail.com>
2022-04-18 22:13:07 +0200

    Wrote units tests for add_item method

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,4 +1,5 @@
-require './classes/author'
+require_relative '../classes/author'
+require_relative '../classes/item'
 
 describe Author do
   before :each do
@@ -18,6 +19,22 @@ describe Author do
     it 'should have a first_name instance variable' do
       l_name = @author.last_name
       expect(l_name).to eq 'Kutz'
+    end
+  end
+
+  context 'testing instance methods' do
+    it 'increase author items count by 1' do
+      author_items = @author.items.size
+      item_instance = Item.new('2011/10/01', false, @author)
+      @author.add_item(item_instance)
+      expect(items).to eq (items + 1)
+    end
+
+    it 'item author should be instance of Item class' do
+      item_instance = Item.new('2011/10/01', false, @author)
+      @author.add_item(item_instance)
+      author_item = @author.item.first
+      expect(author).to be instance_of Item
     end
   end
 end
