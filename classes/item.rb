c61d20625404d433d982f5a6b65ac28eac9fb013
clintonjosephs <clintonjosephs26@gmail.com>
2022-04-18 14:15:41 +0100

    wrote tests for the can_be _archived?() method

--- classes/item.rb
+++ classes/item.rb
@@ -1,10 +1,12 @@
+require 'date'
 
 class Item
     attr_reader :id, :archived
     attr_accessor :publish_date
     def initialize(publish_date, archived)
         @id = rand(1..100)
-        @publish_date = publish_date
+        @publish_date = Date.parse(publish_date)
         @archived = archived
     end
+    
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -10,12 +10,27 @@ describe Item do
             expect(@item).to be_an_instance_of Item
         end
 
+        it 'item published date should be of type Date' do
+            expect(@item.publish_date).to be_an_instance_of Date
+        end
+
         it 'should have a date' do
-            expect(@item.publish_date).to eq('2011/10/01')
+            expect(@item.publish_date.to_s).to eq('2011-10-01')
         end
 
         it 'should have a archived' do
             expect(@item.archived).to eq(false)
+        end
+    end
+
+    context 'test methods in the items class to' do
+        it 'can_be_archived? method should' do
+            expect(@item.can_be_archived?).to eq(true)
+        end 
+
+        it 'can_be_archived? method should return false' do
+            item_instance = Item.new('2020/10/01', true)
+            expect(item_instance.can_be_archived?).to eq(false)
         end
     end
 end
