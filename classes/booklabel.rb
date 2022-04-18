9d7c7354df5c05704f6e2a60dc9eaa8d2571c1f8
joskalenda <joskalenda3@gmail.com>
2022-04-19 01:09:55 +0200

    Add label book class

---
+++ classes/booklabel.rb
@@ -0,0 +1,17 @@
+require './item'
+
+class Label
+  attr_reader :title, :color, :items, :id
+
+  def initialize(title, color)
+    @id = rand(0..50)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    @item.add_label(self)
+  end
+end
