5250eda3d4d67ada4efd10b2c5e600279e5db5d6
clintonjosephs <clintonjosephs26@gmail.com>
2022-04-18 20:50:22 +0100

    fixed linters errors

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,17 +1,18 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
+  attr_accessor :on_spotify
 
-    def initialize(publish_date, archived, on_spotify)
-        super(publish_date, archived)
-        @on_spotify = on_spotify
-    end
+  def initialize(publish_date, archived, on_spotify)
+    super(publish_date, archived)
+    @on_spotify = on_spotify
+  end
 
-    def can_be_archived?
-        return true if super && on_spotify
-        false
-    end
+  def can_be_archived?
+    return true if super && on_spotify
 
-    private :can_be_archived?
+    false
+  end
+
+  private :can_be_archived?
 end
