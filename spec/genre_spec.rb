062e20d48e45a3c59422fac418de0ccd49dc83f2
clintonjosephs <clintonjosephs26@gmail.com>
2022-04-18 21:17:38 +0100

    added more tests to the genre class

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,8 +1,9 @@
-require_relative './classes/genre'
+require_relative '../classes/genre'
 
 describe Genre do
     before(:all) do
         @genre = Genre.new('Comedy')
+        @item = Item.new('2011/10/01', false)
     end
 
     it 'should be an instance of Genre' do
@@ -15,5 +16,20 @@ describe Genre do
 
     it 'should have a list of items' do
         expect(@genre.items).to be_an_instance_of Array
+    end
+
+    context 'test relationship between genre and item' do
+        it 'should add an item to the genre' do
+            @genre.add_item(@item)
+            expect(@genre.items).to include(@item)
+        end
+
+        it 'should have atleast one item in the genre' do
+            expect(@genre.items.length).to be > 0
+        end
+
+        it 'item should have comedy as its genre' do
+            expect(@item.genre.name).to eq('Comedy')
+        end
     end
 end
