6ae5ef844aa62ef6ac1905274d86c9c857de4a92
AdornChoga <adornchoga@gmail.com>
2022-04-18 21:46:51 +0200

    Add id and items instance variables

--- classes/author.rb
+++ classes/author.rb
@@ -4,7 +4,9 @@ class Author < Item
   attr_accessor :first_name, :last_name
   def initialize(first_name, last_name, publish_date, archived)
     super(publish_date, archived)
+    @id = Math.rand(1...100)
     @first_name = first_name
     @last_name = last_name
+    @items = []
   end
 end
