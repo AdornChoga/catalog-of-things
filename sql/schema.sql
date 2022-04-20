d09029ae6f09e2df2ea71658b39b68695c3f505f
clintonjosephs <clintonjosephs26@gmail.com>
2022-04-20 17:53:35 +0100

    made changes to schema file

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,20 +4,11 @@ CREATE TABLE music_album (
         published_date BOOLEAN, 
         archived BOOLEAN, 
         on_spotify BOOLEAN, 
+        genre_id INTEGER,
+        CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE CASCADE
     PRIMARY KEY(id) 
 );
 /* Create music_album table end */
-
-/* Create album_genres adjoint table start */
-CREATE TABLE album_genres ( 
-            id SERIAL PRIMARY KEY, 
-            music_album_id INTEGER, 
-            genre_id INTEGER, 
-        CONSTRAINT unique_music_album_id UNIQUE(music_album_id),
-        CONSTRAINT fk_album FOREIGN KEY (music_album_id) REFERENCES music_album(id) ON DELETE CASCADE, 
-        CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE CASCADE
-    );
-/* Create album_genres adjoint table end */
 
 /* Create genre table start */
 CREATE TABLE genre (
